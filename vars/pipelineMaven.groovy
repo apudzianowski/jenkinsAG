9e1d4e20ba05d5a5508ee83afe0d1726f8964242
Andrzej Gusciora <andrzej.gusciora@onwelo.com>
2023-06-12 08:42:35 +0200

    added gradle and modified pipeline

--- vars/pipelineMaven.groovy
+++ vars/pipelineMaven.groovy
@@ -1,3 +1,23 @@
-def call(String name = 'human') {
-    echo "Hello, ${name}."
+def call (Map config = [:]) {
+    defaultMap = [moduleName: 'works', enviroment: 'bar', repoName: 'jenkinsAG']
+    config = defaultMap << config
+
+    node{
+        stage ('Download source code'){
+            checkout ([$class: 'GitSCM', branches: [[name: '*/main']], userRemoteConfigs:[[url: "https://github.com/apudzianowski/spring-petclinic"]]])
+        }
+        stage ('Building source code'){
+                sh 'mvn -B -DskipTests clean package'
+            }
+        stage ('Test app') {
+                sh 'mvn verify'
+            post {
+                always{
+                    junit 'target/surefire-reports/*.xml'
+                }
+            }
+        }
+        stage ('Installing Artifacts') {
+            sh 'mvn install -DskipTests'
+        }
 }
