d35a9d83c1bee293fe1833ba55747d217e1e20b0
Andrzej Gusciora <andrzej.gusciora@onwelo.com>
2023-06-13 13:20:30 +0200

    final7

--- vars/pipelineMaven.groovy
+++ vars/pipelineMaven.groovy
@@ -27,12 +27,6 @@ def call (Map config = [:]) {
             if (!skipInstall){
             sh 'mvn install -DskipTests'
             }
-        
-        stage ('Clean The Mess') {
-        // Clean after build
-                echo '\033[33mCzyszcze\033[0m \033[33mci\033[0m \033[33mrure!\033[0m'
-                cleanWs()
-    }
         }
     }
     }
